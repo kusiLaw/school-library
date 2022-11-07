38f5dc57d0e7679630bf01b9f2db7ce46df47ddb
kusi <lawcubegsm@gmail.com>
2022-11-07 18:52:04 +0000

    Fix linter error

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unkown', parent_permission = true)
+  def initialize(age, specialization, name = 'Unkown', parent_permission = true) # rubocop:disable Style/OptionalBooleanParameter
     super(age, name, parent_permission)
     @specialization = specialization
   end
