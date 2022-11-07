3df1fb2a4f9d69226b1987b4a9ba5ab2cd54d5c5
kusi <lawcubegsm@gmail.com>
2022-11-07 19:17:19 +0000

    Fix logic error

--- person.rb
+++ person.rb
@@ -9,11 +9,13 @@ class Person
     @parent_permission = parent_permission
   end
 
-  def of_age?
-    @age >= 18
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
-  def can_use_services?
-    of_age? && @parent_permission
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
