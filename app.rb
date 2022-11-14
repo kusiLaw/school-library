f223ad8332a66a38ffc0652d644e92232b9afad3
kusi <lawcubegsm@gmail.com>
2022-11-14 12:58:27 +0000

    Refactor create methods of app into module
    - Add rentals to attr_accessor
    - integrated welcome commands into main methods

--- app.rb
+++ app.rb
@@ -5,9 +5,11 @@ require './rentals'
 require './person'
 require './teacher'
 require './list_module'
+require './create_module'
 
 class App
   include List
+  include Create
 
   attr_accessor :cache, :terminate
 
@@ -30,60 +32,6 @@ class App
       create_rent
     when '6'
       list_rental
-    end
-  end
-
-  private
-
-  def create_book
-    puts 'title'
-    title = gets.chomp
-    puts 'author'
-    author = gets.chomp
-    @cache[:book] = [*@cache[:book], Book.new(title, author)]
-  end
-
-  def create_student
-    puts 'Name?'
-    name = gets.chomp
-    puts 'Age?'
-    age = gets.chomp
-    puts 'Parent permission?[y/n]'
-    permission = gets.chomp
-    @cache[:people] = [*@cache[:people], Student.new(age, '', name, permission.upcase == 'Y')]
-  end
-
-  def create_teacher
-    puts 'Name?'
-    name = gets.chomp
-    puts 'Age?'
-    age = gets.chomp
-    puts 'Specialization?'
-    specialization = gets.chomp
-    @cache[:people] = [*@cache[:people], Teacher.new(age, specialization, name)]
-  end
-
-  def create_person
-    puts 'Do you want to create a student(1) or a teacher (2) for teacher' \
-         '[input a number]'
-    gets.chomp == '1' ? create_student : create_teacher
-  end
-
-  def create_rent
-    puts 'Select a book from a following list by number'
-    list_all_book(true)
-    book_index = gets.chomp
-    puts 'Select a person from the following list by number(not id)'
-    list_all_people(true)
-    person_index = gets.chomp
-    puts 'Date'
-    date = gets.chomp
-    begin
-      person = @cache[:people][book_index.to_i]
-      book = @cache[:book][person_index.to_i]
-      @cache[:rental] = [*@cache[:rental], Rentals.new(date, book, person)]
-    rescue StandardError => e
-      puts "incorrect value #{e}"
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,24 +1,21 @@
 require './app'
 
-def welcome_commands
+def main
   app = App.new
   until app.terminate
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - list all rental for a given person id'
-    puts '7 - Exit'
+    puts 'Please choose an option by entering a number:
+    1 - List all books
+    2 - List all people
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - list all rental for a given person id
+    7 - Exit'
     choice = gets.chomp
+    app.terminate = true if choice == '7'
     app.entry(choice)
   end
   puts 'Thank you for using this app!'
-end
-
-def main
-  welcome_commands
 end
 
 main
